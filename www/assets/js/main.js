c0c344ab7dedf80c041bee9f4ca6d09dd5faff0a
YoheiGoto <90916231+Marcs930@users.noreply.github.com>
2023-02-28 10:33:49 +0900

    spNavのJS実装中

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -2,65 +2,66 @@
 // import Swiper from "swiper/bundle";
 // import "swiper/css/bundle";
 // import "@scss/style";
-// import { forEach } from "core-js/core/array";
+// import { forEach } from "core-./core/array";
 // import utils from './utils/index.js';
 // resolveのmodules設定により、srcフォルダまでパスが通っている
-import "js/utils/header_fixed";
-import "js/utils/spnav";
-import "js/utils/swiper";
-import "js/utils/scroll";
-import "js/utils/side";
-import { ScrollObserver } from "js/utils/scroll";
-import { ScrollToggleClass } from "js/utils/side";
-import { ClickHandler } from "js/utils/spnav";
-import { HeaderFixed } from "js/utils/header_fixed";
+// import "./utils/spnav";
+// import "./utils/header_fixed";
+// import "./utils/swiper";
+// import "./utils/scroll";
+// import "./utils/side";
+// import { ScrollObserver } from "./utils/scroll";
+// import { ScrollToggleClass } from "./utils/side";
+// import { ClickHandler } from "./utils/spnav";
+// import { HeaderFixed } from "./utils/header_fixed";
 
 class Main {
-    constructor() {
-        this.header = document.querySelector(".header");
-        this.main = document.querySelector(".main");
-        this.side = document.querySelector(".side");
-        this._observers = [];
-        this._scrollInit();
-        this._scrollToggleClassInit();
-        this._clickHandlerInit();
-        this._headerFixedInit();
-    }
+  constructor() {
+    this.header = document.querySelector(".header");
+    this.main = document.querySelector(".main");
+    this.side = document.querySelector(".side");
+    this._observers = [];
+    this._clickHandlerInit();
+    // this._scrollInit();
+    // this._scrollToggleClassInit();
+    // this._headerFixedInit();
+  }
 
-    _sideAnimation(el, inview) {
-        if (inview) {
-            this.side.classList.add("inview");
-        } else {
-            this.side.classList.remove("inview");
-        }
-    }
-    _inviewAnimation(el, inview) {
-        if (inview) {
-            el.classList.add("inview");
-        } else {
-            el.classList.remove("inview");
-        }
-    }
-    _scrollInit() {
-        this._observers.push(
-            new ScrollObserver(".appear", this._inviewAnimation, {
-                rootMargin: "0px 0px -100px 0px",
-                once: true,
-            })
-        );
-    }
-    _scrollToggleClassInit() {
-        new ScrollToggleClass(".side", 350);
-    }
-    _clickHandlerInit() {
-        new ClickHandler(".js-spbtn", "header", "js-spnav-active");
-    }
-    _headerFixedInit() {
-        new HeaderFixed();
-    }
+  // _sideAnimation(el, inview) {
+  //     if (inview) {
+  //         this.side.classList.add("inview");
+  //     } else {
+  //         this.side.classList.remove("inview");
+  //     }
+  // }
+  // _inviewAnimation(el, inview) {
+  //     if (inview) {
+  //         el.classList.add("inview");
+  //     } else {
+  //         el.classList.remove("inview");
+  //     }
+  // }
+  // _scrollInit() {
+  //     this._observers.push(
+  //         new ScrollObserver(".appear", this._inviewAnimation, {
+  //             rootMargin: "0px 0px -100px 0px",
+  //             once: true,
+  //         })
+  //     );
+  // }
+  // _scrollToggleClassInit() {
+  //     new ScrollToggleClass(".side", 350);
+  // }
+  _clickHandlerInit() {
+    new ClickHandler(".js-hamburgerBtn", ".container", "is-open");
+  }
+  // _headerFixedInit() {
+  //     new HeaderFixed();
+  // }
 }
 
 new Main();
+
 
 // export class ClickHandler {
 //     constructor(observe, target, className) {
--- www/assets/js/utils/spnav.js
+++ www/assets/js/utils/spnav.js
@@ -1,24 +1,25 @@
-export class ClickHandler {
-    constructor(observe, target, className) {
-        this.observe = document.querySelector(observe);
-        this.target = document.querySelector(target);
-        this.className = className;
-        this._init();
-    }
-    _init() {
-        let scrollPos;
-        this.observe.addEventListener("click", () => {
-            this.target.classList.toggle(this.className);
-            if (this.target.classList.contains(this.className)) {
-                scrollPos = window.pageYOffset;
-                document.body.classList.add("js-spnav-open");
-                document.body.style.top = scrollPos * -1 + "px";
-                console.log("opened");
-            } else {
-                document.body.classList.remove("js-spnav-open");
-                window.scrollTo(0, scrollPos);
-                console.log("closed");
-            }
-        });
-    }
+class ClickHandler {
+  constructor(observe, target, className) {
+    this.observe = document.querySelector(observe);
+    this.target = document.querySelector(target);
+    this.className = className;
+    this._init();
+  }
+  _init() {
+    let scrollPos;
+    this.observe.addEventListener("click", () => {
+      this.target.classList.toggle(this.className);
+      if (this.target.classList.contains(this.className)) {
+        scrollPos = window.pageYOffset;
+        console.log(scrollPos);
+        document.body.classList.add("js-spnav-open");
+        document.body.style.top = scrollPos * -1 + "px";
+        console.log("opened");
+      } else {
+        document.body.classList.remove("js-spnav-open");
+        window.scrollTo(0, scrollPos);
+        console.log("closed");
+      }
+    });
+  }
 }
