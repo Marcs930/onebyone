9d818aff0c2e66c35278cb034cd5053f92d36c3e
YoheiGoto <90916231+Marcs930@users.noreply.github.com>
2023-03-21 02:26:50 +0900

    single-buildingページスタイリング完了_全体残り消化・微修正中

--- www/assets/js/utils/swiper.js
+++ www/assets/js/utils/swiper.js
@@ -1,32 +1,69 @@
 const mvSwiper = new Swiper(".mv__slider", {
-    // Optional parameters
-    direction: "horizontal",
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 30000,
     loop: true,
-    slidesPerView: 1,
-    autoplay: {
-        delay:30000,
-        loop: true,
-    },
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// サムネイル
+const sliderThumbnail = new Swiper(".buildingSlider__thumbnail", {
+  slidesPerView: 5, // サムネイルの枚数
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 6, breakpoints: {
+    // スライドの表示枚数：500px以上の場合
+    768: {
+      slidesPerView: 6,
+    }
+  }
 
 });
-const buildingSwiper = new Swiper(".buildingSlider", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-    // slidesPerView: 1,
-    // autoplay: {
-    //     delay:30000,
-    //     loop: true,
-    // },
-    // // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+// スライダー
+// const slider = new Swiper(".slider", {
+//   loop: true,
+//   // 前後の矢印
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: sliderThumbnail,
+//   },
+// });
 
+const buildingSwiper = new Swiper(".buildingSlider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 30000,
+    loop: true,
+  },
+  // If we need pagination
+  // pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  // },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
 });
